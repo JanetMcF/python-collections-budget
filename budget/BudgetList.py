87cb63365d80165ba5d14cbbb2c80523dac14e94
JanetMcF <82966128+JanetMcF@users.noreply.github.com>
2022-02-23 04:50:17 -0700

    Update BudgetList.py

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -24,8 +24,8 @@ class BudgetList:
         return len(self.expenses) + len(self.overages)
 
     def __iter__(self):
-        iter_e = iter(self.expenses)
-        iter_o = iter(self.overages)
+        self.iter_e = iter(self.expenses)
+        self.iter_o = iter(self.overages)
         return self
 
     def __next__(self):
